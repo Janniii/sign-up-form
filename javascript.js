65491cdb698ad88ba28af36257ff5c42fa7a9203
Janni <36205116+Janniii@users.noreply.github.com>
2022-10-12 23:11:01 +0200

    Improve modal animation, functionality, add new asset

--- javascript.js
+++ javascript.js
@@ -7,23 +7,28 @@ const body = document.querySelector("body");
 const container = document.querySelector(".container")
 
 
+let itemList = []
+
 
 btn.addEventListener("click", () => {
     console.log("hello");
 
         inputs.forEach(input  => {
 
-            console.log(input.checked);
-
-
             if (input.type != "radio" && input.type != "checkbox") {
                 console.log(input.value)
+                if (input.value != "") {
+                    itemList.push(input.value);
+                }
             }
             
             
             
             else if (input.checked) {
                 console.log(input.value, input.checked)
+                if (input.value != "") {
+                    itemList.push(input.value);
+                }
             }
 
         })
@@ -43,6 +48,12 @@ modalBack.addEventListener("click", (event) => {
     if (event.target == modalBack || event.target == closebtn) {
         modal.style.display = "none";
         modalBack.style.display = "none";
+        itemList = [];
+
+        const oldP = document.querySelectorAll(".temporary");
+        console.log(oldP);
+        oldP.forEach(old => old.remove());
+        console.log(oldP);
     }
     
     
@@ -52,6 +63,8 @@ modalBack.addEventListener("click", (event) => {
 
 
 const closebtn = document.querySelector(".close");
+
+const modalContainer = document.querySelector(".modal-container")
 
 
 
@@ -66,7 +79,13 @@ function createWindow() {
     windowyo.setAttribute("style", "z-index: 1; display: block; width: 400px; height: 600px; background-color: red; position: absolute; left: calc(1920px - 60%); top: calc(860px - 80%); display: flex; align-self: center;")
     body.appendChild(windowyo); */
 
+    for (let i = 0; i < itemList.length; i++) {
+        const newP = document.createElement("p");
+        newP.textContent = itemList[i];
+        newP.classList.add("temporary");
+        modalContainer.appendChild(newP);
 
+    }
 
 }
 
